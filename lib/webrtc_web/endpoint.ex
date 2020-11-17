a3330ea7e2332a415db59a0062d42e15d9902001
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2020-11-17 18:32:47 +0530

    heroku setup

--- lib/webrtc_web/endpoint.ex
+++ lib/webrtc_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule WebrtcWeb.Endpoint do
   ]
 
   socket "/socket", WebrtcWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
